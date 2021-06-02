fb70de00bc88402d4e6bc94f70efd82ebbd23443
熊静松 <8238536+xiong-jingsong@user.noreply.gitee.com>
2021-06-02 22:10:41 +0800

    优化创建校验

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -11,32 +11,13 @@ interface SignData {
 
 const Posts: NextApiHandler = async (req, res,) => {
     const { username, password, passwordConfirmation } = req.body as SignData
-
-    const errors = { username: [] as string[], password: [] as string[], passwordConfirmation: [] as string[] }
-
-    if (username.trim() === '') {
-        errors.username.push('不能为空')
-    }
-    if (!/[a-zA-z0-9]/.test(username.trim())) {
-        errors.username.push('格式错误')
-    }
-    if (username.trim().length > 42) {
-        errors.username.push('超过最大长度')
-    }
-    if (username.trim().length <= 3) {
-        errors.username.push('低于最小长度')
-    }
-    if (password === '') {
-        errors.password.push('不能为空')
-    }
-    if (passwordConfirmation !== password) {
-        errors.passwordConfirmation.push('两次密码不一致')
-    }
-    const hasErrors = Object.values(errors).find(v => v.length > 0)
-    if (hasErrors) {
-        console.log('是这里----')
-        console.log(errors)
-        res.status(422).json(errors)
+    const user = new User()
+    user.username = username
+    user.password = password
+    user.passwordConfirmation = passwordConfirmation
+    await user.validate()
+    if (user.hasErrors()) {
+        res.status(422).json(user.errors)
     } else {
         const connect = await getDatabaseConnection()
         const user = new User()
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,6 +1,8 @@
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Post } from "./Post";
 import { Comment } from "./Comment";
+import { getDatabaseConnection } from "lib/getDatabaseConnection";
+
 
 @Entity('users')
 export class User {
@@ -18,4 +20,38 @@ export class User {
     posts: Post[]
     @OneToMany(type => Comment, comment => comment.user)
     comments: Comment[]
+    errors = { username: [] as string[], password: [] as string[], passwordConfirmation: [] as string[] }
+    passwordConfirmation: string
+    password: string
+
+    async validate() {
+
+        if (this.username.trim() === '') {
+            this.errors.username.push('不能为空')
+        }
+        if (!/[a-zA-z0-9]/.test(this.username.trim())) {
+            this.errors.username.push('格式错误')
+        }
+        if (this.username.trim().length > 42) {
+            this.errors.username.push('超过最大长度')
+        }
+        if (this.username.trim().length <= 3) {
+            this.errors.username.push('低于最小长度')
+        }
+        const found = (await (await getDatabaseConnection()).manager.find(User, { username: this.username }))
+        if (found.length > 0) {
+            this.errors.username.push('用户名已存在')
+        }
+        if (this.password === '') {
+            this.errors.password.push('不能为空')
+        }
+        if (this.passwordConfirmation !== this.password) {
+            this.errors.passwordConfirmation.push('两次密码不一致')
+        }
+    }
+    hasErrors() {
+        console.log('this.errors-----------')
+        console.log(this.errors)
+        return (!!Object.values(this.errors).find(v => v.length > 0))
+    }
 }
