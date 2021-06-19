ccd17eadbce97ec2a7348d5739690e0dd41ad987
熊静松 <8238536+xiong-jingsong@user.noreply.gitee.com>
2021-06-19 18:37:16 +0800

    配置

--- lib/getDatabaseConnection.ts
+++ lib/getDatabaseConnection.ts
@@ -9,7 +9,6 @@ const create = async () => {
     //@ts-ignore
     return createConnection({
         ...config,
-        host: process.env.NODE_ENV === 'production' ? 'localhost' : 'host.docker.internal',
         database: process.env.NODE_ENV === 'production' ? 'production_blog' : 'development_blog',
         entities: [Post, User, Comment]
     })
