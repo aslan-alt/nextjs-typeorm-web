1b3e0cd96cd5573c9566af3a94722aaac56b1f23
jingsong.xiong <874470891@qq.com>
2023-02-17 21:39:54 +0800

    Patch:test

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -3,6 +3,6 @@ export const ironOptions = {
   password: '6f85853e-6922-432e-8022-9be8bceb521d',
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
   },
 };
