a5ee9cfe6d2016cee92569e737858c777c7c69fb
熊静松 <874470891@qq.com>
2023-10-21 12:21:56 +0800

    Patch:Solve flickering problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const withImages = require('next-images')
-module.exports = withImages({
-  webpack(config, options) {
-    return config
-  }
-})
+/** @type {import('next').NextConfig} */
+const withImages = require('next-images');
+
+module.exports = {
+  ...withImages({
+    webpack(config) {
+      return config;
+    },
+  }),
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+};
