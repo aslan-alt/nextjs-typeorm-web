19095f071cac2f4160df97b441a52b58d879ba80
熊静松 <874470891@qq.com>
2023-11-01 15:47:13 +0800

    Patch: update .env

--- pages/api/getAllComment.ts
+++ pages/api/getAllComment.ts
@@ -1,21 +1,32 @@
 import {NextApiHandler} from 'next';
+import {getServerSession} from 'next-auth';
 import {UAParser} from 'ua-parser-js';
 import {Comment} from '@database/entity/Comment';
 import {getConnection} from '@database/getConnection';
 import deepClone from '@lib/deepClone';
+import {authOptions} from './auth/[...nextauth]';
 
 const GetAllComments: NextApiHandler = async (req, res) => {
-  const ua = req.headers['user-agent'];
-  const result = new UAParser(ua).getResult();
-  const connection = await getConnection();
-
-  const found = (await connection.manager.find(Comment)).sort(function (a, b) {
-    return a.createdAt < b.createdAt ? 1 : -1;
-  });
+  // const ua = req.headers['user-agent'];
+  // const result = new UAParser(ua).getResult();
+  // const connection = await getConnection();
+  //
+  // const found = (await connection.manager.find(Comment)).sort(function (a, b) {
+  //   return a.createdAt < b.createdAt ? 1 : -1;
+  // });
+  const session = await getServerSession(req, res, authOptions);
+  console.log('session-------');
+  console.log(session);
+  if (!session) {
+    res.status(401).json({message: 'You must be logged in.'});
+    return;
+  }
 
   await res.status(200).send({
-    comments: deepClone(found),
-    result: deepClone(result),
+    comments: [],
+    result: {
+      message: 'Success',
+    },
   });
 };
 export default GetAllComments;
